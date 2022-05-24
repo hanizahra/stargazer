7322be48609e04ce0b16072e7280a154a1da3268
Hani Zahra <hzahra@revation.com>
2022-05-24 00:07:23 -0700

    added list items test

--- src/components/topicList/TopicList.test.js
+++ src/components/topicList/TopicList.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import TopicList from './TopicList';
 import mockResponse from '../../tests/mockResponse';
 
-test('renders learn TopicList component', () => {
+test('renders learn TopicList component', async () => {
   render(<TopicList />);
   const zeroResultElement = screen.getByText(/0 results/i);
   expect(zeroResultElement).toBeInTheDocument();
@@ -11,4 +11,6 @@ test('renders learn TopicList component', () => {
   );
   const resultElement = screen.getByText(/react-native/i);
   expect(resultElement).toBeInTheDocument();
+  const listItems = await screen.findAllByRole('listitem');
+  expect(listItems).toHaveLength(mockResponse.relatedTopics.length+1);
 });
